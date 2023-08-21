14981abe102656db847512e1f7faccce2dee8dac
Doviet992001 <dohoangviet992001@gmail.com>
2023-08-21 19:56:16 +0700

    update

--- lib/constants/fetch_promotion.dart
+++ lib/constants/fetch_promotion.dart
@@ -6,7 +6,7 @@ class FetchPromotion {
   final String urlRead = "http://45.117.170.206:60/apis/promotion";
   final int statusCode200 = 200;
   final String bearerToken =
-      "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ2b3RpZW4xMjM0NUBnbWFpbC5jb20iLCJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUm9sZV9DdXN0b21lciJ9XSwiaWF0IjoxNjkyNTk5MTE5LCJleHAiOjE2OTI2MTcxMTl9.4SFeybY4rIgL1EuPlbxTDWmObc2xBUZiBfSoUNulagw";
+      "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ2b3RpZW4xMjM0NUBnbWFpbC5jb20iLCJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUm9sZV9DdXN0b21lciJ9XSwiaWF0IjoxNjkyNjIyMDIyLCJleHAiOjE2OTI2NDAwMjJ9.HACqVd9cT6zYY_I-Z32eNqzrAMSuJQAfitxGeabzCfs";
 
   List<Promotion> parsePromotions(String response) {
     final jsonMap = jsonDecode(response);
--- lib/pages/information_page.dart
+++ lib/pages/information_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_store/constants/size_config.dart';
+import 'package:mobile_store/cubit/app_cubit_states.dart';
+import 'package:mobile_store/cubit/app_cubits.dart';
 import 'package:mobile_store/widgets/information_tab.dart';
 import 'package:mobile_store/widgets/order_tab.dart';
 import '../widgets/custom_app_bar.dart';
@@ -34,86 +37,95 @@ class _InformationPage extends State<InformationPage>
   Widget build(BuildContext context) {
     SizeConfig().init(context);
 
-    return Scaffold(
-        backgroundColor: Color(0xFFE0EAEB),
-        // appBar: AppBar(title: const Text('Mobile Store'),),
-        appBar: CustomAppBar(
-          context: context,
-          title: 'Mobile Store',
-          logged: false,
-        ),
-        body: Container(
-          margin: const EdgeInsets.all(9),
-          width: double.maxFinite,
-          height: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 84,
-                width: double.maxFinite,
-                margin: const EdgeInsets.only(left: 2.0, right: 2.0),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    topRight: Radius.circular(10.0),
-                  ),
-                ),
-                child: ListView.builder(
-                  itemCount: 3,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          currentTab = index;
-                        });
-                      },
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 300),
-                        margin:
-                            const EdgeInsets.only(top: 6, bottom: 6, left: 6),
-                        width: MediaQuery.of(context).size.width * 0.3,
-                        height: double.maxFinite,
-                        decoration: const BoxDecoration(color: Colors.white),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                                'assets/img/' + itemsImagesInformation[index]),
-                            Text(itemsTextInformation[index])
-                          ],
-                        ),
+    return BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
+      if (state is UserLoadedState) {
+        return Scaffold(
+            backgroundColor: Color(0xFFE0EAEB),
+            // appBar: AppBar(title: const Text('Mobile Store'),),
+            appBar: CustomAppBar(
+              logged: true,
+              title: '',
+              showUserInfo: true,
+              context: context,
+              user: state.user,
+            ),
+            body: Container(
+              margin: const EdgeInsets.all(9),
+              width: double.maxFinite,
+              height: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 84,
+                    width: double.maxFinite,
+                    margin: const EdgeInsets.only(left: 2.0, right: 2.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0),
                       ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              Expanded(
-                child: Container(
-                  height: 84,
-                  width: double.maxFinite,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20.0),
-                      bottomRight: Radius.circular(20.0),
+                    ),
+                    child: ListView.builder(
+                      itemCount: 3,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              currentTab = index;
+                            });
+                          },
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            margin: const EdgeInsets.only(
+                                top: 6, bottom: 6, left: 6),
+                            width: MediaQuery.of(context).size.width * 0.3,
+                            height: double.maxFinite,
+                            decoration:
+                                const BoxDecoration(color: Colors.white),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset('assets/img/' +
+                                    itemsImagesInformation[index]),
+                                Text(itemsTextInformation[index])
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      tabPages[currentTab],
-                    ],
+                  const SizedBox(
+                    height: 6,
                   ),
-                ),
+                  Expanded(
+                    child: Container(
+                      height: 84,
+                      width: double.maxFinite,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20.0),
+                          bottomRight: Radius.circular(20.0),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          tabPages[currentTab],
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ));
+            ));
+      } else {
+        return const Center(child: CircularProgressIndicator());
+      }
+    });
   }
 }

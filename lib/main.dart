1401de4df9a8519831d668ce2f97daddcb891d9c
văn hải <74531274+VanHai18287@users.noreply.github.com>
2023-08-23 19:41:16 +0700

    commit checkout 90

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mobile_store/constants/color_const.dart';
 import 'package:mobile_store/cubit/auth_cubit/app_cubit_logic.dart';
+import 'package:mobile_store/pages/check_out_page.dart';
 import 'package:mobile_store/pages/login_page.dart';
 import 'package:mobile_store/pages/home_page.dart';
 import 'package:mobile_store/pages/information_page.dart';
@@ -11,6 +12,13 @@ import 'package:mobile_store/services/user_data_services.dart';
 
 import 'cubit/app_cubits.dart';
 import 'models/api_user.dart';
+
+// void main() {
+//   // runApp(InformationPage());
+//   runApp(
+//   MaterialApp(debugShowCheckedModeBanner: false, home: const CheckOutPage()));
+// }
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();

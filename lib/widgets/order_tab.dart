302980f4db638348c9b387e58ae28fe3df3f2e47
Duy Duc Nguyen <duyduck2809@gmail.com>
2023-09-01 16:20:02 +0700

    Update order_tab.dart

--- lib/widgets/order_tab.dart
+++ lib/widgets/order_tab.dart
@@ -26,7 +26,7 @@ class _OrderTabState extends State<OrderTab> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 6),
-      margin: const EdgeInsets.symmetric(horizontal: 9, vertical: 9),
+      margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 9),
       color: Colors.white,
       child: FutureBuilder(
           future: futureListOrder,
@@ -35,92 +35,102 @@ class _OrderTabState extends State<OrderTab> {
               return Text('Retrive Failed${snapshot.error}');
             } else if (snapshot.hasData) {
               final List<Order> orders = snapshot.data!;
-              return SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 8,
-                  ),
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    itemCount: orders.length,
-                    itemBuilder: (context, index) => ListTile(
-                      tileColor: Colors.white,
-                      leading: Expanded(
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                          ),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 95,
-                                  height: 48,
-                                  child: Image.network(
-                                    FetchOrder.baseUrl +
-                                        ('${orders[index].productOrderDTO?.image}'),
-                                  ),
+              return Container(
+                color: Colors.white,
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                width: double.maxFinite,
+                height: MediaQuery.of(context).size.height * 0.56,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: orders.length,
+                          itemBuilder: (context, index) => ListTile(
+                            tileColor: Colors.white,
+                            leading: Expanded(
+                              child: Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
                                 ),
-                              ]),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        width: 95,
+                                        height: 48,
+                                        child: Image.network(
+                                          FetchOrder.baseUrl +
+                                              ('${orders[index].productOrderDTO?.image}'),
+                                        ),
+                                      ),
+                                    ]),
+                              ),
+                            ),
+                            title: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${orders[index].productOrderDTO?.name}',
+                                    style: const TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    'Delivery date: ${orders[index].receiveDate}',
+                                    style: const TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                  const SizedBox(
+                                    height: 2,
+                                  ),
+                                  Text(
+                                    'Order number: ${orders[index].productOrderDTO?.seri}',
+                                    style: const TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                ]),
+                            subtitle: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '${formatNumber(orders[index].total!.toInt())} VND',
+                                  style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.green),
+                                ),
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      backgroundColor: ColorPallete.mainColor,
+                                      fixedSize: const Size(80, 20)),
+                                  child: const Text(
+                                    'Detail',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                  onPressed: () {},
+                                )
+                              ],
+                            ),
+                          ),
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
                         ),
                       ),
-                      title: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${orders[index].productOrderDTO?.name}',
-                              style: const TextStyle(
-                                  fontSize: 12, fontWeight: FontWeight.w600),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Delivery date: ${orders[index].receiveDate}',
-                              style: const TextStyle(
-                                  fontSize: 10, fontWeight: FontWeight.w300),
-                            ),
-                            const SizedBox(
-                              height: 2,
-                            ),
-                            Text(
-                              'Order number: ${orders[index].productOrderDTO?.seri}',
-                              style: const TextStyle(
-                                  fontSize: 10, fontWeight: FontWeight.w300),
-                            ),
-                          ]),
-                      subtitle: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            '${formatNumber(orders[index].total!.toInt())} VND',
-                            style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.green),
-                          ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(5)),
-                                backgroundColor: ColorPallete.mainColor,
-                                fixedSize: const Size(80, 20)),
-                            child: const Text(
-                              'Detail',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            onPressed: () {},
-                          )
-                        ],
-                      ),
-                    ),
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                  ),
-                ),
+                    ]),
               );
             } else {
               return const CircularProgressIndicator();
